e393eb26eddfa534b1c6edaf08e72d7cb2872b05
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2025-11-02 13:27:46 +0900

    update

--- apps/my-fullstack-app/src/app/app.config.ts
+++ apps/my-fullstack-app/src/app/app.config.ts
@@ -2,14 +2,17 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
+  importProvidersFrom, // 👈 ← この1行追加！
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { HttpClientModule } from '@angular/common/http'; // 👈 ← これも追加！
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    importProvidersFrom(HttpClientModule), // 👈 ← これを追加！
   ],
 };
